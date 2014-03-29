3b4d3b8614c9c90cdfe1ec463cdc8af76f8b6194
bbqduc <brunoankka@gmail.com>
2014-03-29 11:42:28 +0200

    chatbox working

--- client/main.js
+++ client/main.js
@@ -4,13 +4,17 @@ require(["imageloader", "tile", "input", "map"], function(imageloader, Tile, inp
 
     var canvas = document.getElementById("gameCanvas");
     var ctx = canvas.getContext("2d");
-    var chatctx = document.getElementById("chatCanvas").getContext("2d");
+    var chatcanvas = document.getElementById("chatCanvas");
+    var chatctx = chatcanvas.getContext("2d");
+    chatctx.fillstyle = "blue"
+    chatctx.font = "bold 16px Arial"
 
     input.initInput();
 
     imageloader.loadImages(function()
     {
         var map = new Map();
+        var chatmessages = []
 
         var ws = new WebSocket("ws://localhost:8000");
         ws.onopen = function(event)
@@ -22,6 +26,30 @@ require(["imageloader", "tile", "input", "map"], function(imageloader, Tile, inp
             ws.send(msgpack.encode({type: 'move', xdir: '1', ydir: '1000' }))
         };
 
+        var redrawChatBox = function()
+        {
+            chatctx.clearRect(0, 0, chatcanvas.width, chatcanvas.height)
+            for(var i = 1; i <= chatmessages.length; ++i)
+            {
+                chatctx.fillText(chatmessages[chatmessages.length-i], 0, chatcanvas.height-16*i);
+            }
+
+        }
+
+        var handleTapiMessage = function(message)
+        {
+            switch(message.type)
+            {
+                case 'chat':
+                    if(message.sender == undefined)
+                        message.sender = "[SERVER]"
+                    chatmessages.push(message.sender + ": " + message.message);
+                    redrawChatBox();
+                    break;
+            }
+
+        };
+
         ws.onmessage = function(event)
         {
             var arrayBuffer;
@@ -31,6 +59,7 @@ require(["imageloader", "tile", "input", "map"], function(imageloader, Tile, inp
                     var msg = msgpack.decode(arrayBuffer);
                     console.log("Received message : " + msg);
                     console.log("chat message : " + msg.message);
+                    handleTapiMessage(msg)
             };
             fileReader.readAsArrayBuffer(event.data);
         };
